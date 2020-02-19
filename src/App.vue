2f096d3dbf844792decb9f18cfbac7562f3b8dd4
11007166 <195812208@qq.com>
2020-02-19 20:54:03 +0800

    feat(初始化计数器项目的基本结构): 初始化计数器项目的基本结构
    初始化计数器项目的基本结构

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,25 @@
 <template>
-  <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div>
+    <my-Addition></my-Addition>
+
+    <p>----------------------</p>
+    <my-Subtraction></my-Subtraction>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import Addition from './components/Addition'
+import Subtraction from './components/Subtraction'
 
 export default {
-  name: 'App',
+  data () {
+    return {
+
+    }
+  },
   components: {
-    HelloWorld
+    'my-Addition': Addition,
+    'my-Subtraction': Subtraction
   }
 }
 </script>
-
-<style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
-</style>
